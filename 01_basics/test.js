7e139647e84ecc38ae56d73dacc738423c326784
Gauri Maheshwari <127858259+Gauri-ma@users.noreply.github.com>
2023-11-26 16:23:18 +0000

    datatypes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -2,3 +2,10 @@ const accountID = 854333
 let accountEmail = "gauri@gmail.com"
 var accountPassword ="1234"
 accountCity = "Aligarh"
+
+
+accountEmail = "gh@hj.com"
+accountPassword = "212121"
+accountCity ="agra"
+
+console.table([accountID, accountEmail, accountPassword,accountCity])
